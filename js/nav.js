beaa3aa864f7d603a98f191ca0cf5f21cc5fde2d
Quentin Marbois <quentin.marbois@gmail.com>
2021-04-12 13:08:39 +0200

    Burguer menu débuggé

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,6 @@
 const toggleMenu = document.querySelector('.toggle-menu');
 const menu = document.querySelector('#menu');
 
-
 toggleMenu.addEventListener('click', function() {
   const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
   toggleMenu.setAttribute('aria-expanded', !open);
